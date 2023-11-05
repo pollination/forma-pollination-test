e06f4bf652f08c238d0a2edc0b4df147d08a40b8
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2023-11-05 01:40:56 -0500

    visualize output

--- assets/forma-binding.js
+++ assets/forma-binding.js
@@ -1,18 +1,31 @@
+const scale = [
+  [0, 0, 255, 255],
+  [135, 206, 250, 255],
+  [0, 255, 255, 255],
+  [0, 128, 128, 255],
+  [0, 255, 0, 255],
+  [255, 255, 0, 255],
+ [255, 165, 0, 255],
+  [255, 0, 0, 255]
+]
+
+const max = 2;
+
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
   forma: {
     getVolumes: function (v) {
-      console.log(123);
-      const geoApi = new EXPERIMENTAL_GeometryApi(Forma.getIframeMessenger());
       return new Promise(async (resolve) => {
         const urn = await Forma.proposal.getRootUrn();
         const paths = await Forma.selection.getSelection();
 
-        const triangles = await Forma.geometry.getTriangles({
-          path: "root",
-          excludedPaths: [paths[0]],
-        });
+        const triangles = [
+          ...(await Forma.geometry.getTriangles({
+            path: "root"
+          })),
+        ];
 
         if (paths.length) {
+          const geoApi = new EXPERIMENTAL_GeometryApi(Forma.getIframeMessenger());
           const volumes = await geoApi.EXPERIMENTAL_getVolume25DCollection({
             urn,
             path: paths[0],
@@ -24,5 +37,55 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
         }
       });
     },
+    vizualize: function (result, data) {
+      return new Promise(async (resolve) => {
+      const resolution = 0.5;
+      const position = [];
+      const colors = [];
+
+      for (let sensor of data) {
+        const {x, y, z, v} = sensor
+
+        position.push(
+          x - resolution,
+          y - resolution,
+          z,
+          x + resolution,
+          y - resolution,
+          z,
+          x - resolution,
+          y + resolution,
+          z,
+          x - resolution,
+          y + resolution,
+          z,
+          x + resolution,
+          y - resolution,
+          z,
+          x + resolution,
+          y + resolution,
+          z
+        );
+
+        const idx = Math.min(Math.floor(v / max * scale.length), scale.length - 1);
+
+        colors.push(...scale[idx], ...scale[idx], ...scale[idx],
+          ...scale[idx],
+          ...scale[idx],
+          ...scale[idx]
+        );
+      }
+
+      await Forma.render.addMesh({ geometryData: { 
+        position: new Float32Array(position), 
+        color: new Uint8Array(colors) 
+      }})
+
+      resolve();
+
+    });
+
+
+    }
   },
 });
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from dash import Dash, dcc, html, Input, Output, clientside_callback, ClientsideFunction
-
+import json
+from dash import Dash, dcc, html, Input, Output, clientside_callback, callback, ClientsideFunction, State
+from dash.exceptions import PreventUpdate
 app = Dash(__name__, external_scripts=[{"src": "./static/forma.mjs", "type": "module"}])
+
 
 # external_scripts = [{"src": "./assets/forma.js", "type": "module"}]
 
@@ -12,14 +14,34 @@ app.layout = html.Div(
         html.Button("Submit", id="button", n_clicks=0),
         html.Br(),
         html.Div(id="my-output"),
+        dcc.Store(id="geometry"),
+        dcc.Store(id="result", storage_type='local'),
+        dcc.Store(id="void")
     ]
 )
 
 clientside_callback(
     ClientsideFunction(namespace="forma", function_name="getVolumes"),
-    Output("my-output", "children"),
+    Output("geometry", "data"),
     [Input("button", "n_clicks")],
 )
+
+clientside_callback(
+    ClientsideFunction(namespace="forma", function_name="vizualize"),
+    Output("void", "data"),
+    Input("result", "modified_timestamp"),
+    State("result", "data"),
+)
+
+@callback(Output("result", 'data'),
+            Input("geometry", 'modified_timestamp'),
+            State("geometry", 'data'))
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+    
+    with open("./results.json", "r") as f:
+        return json.load(f)
 
 
 if __name__ == "__main__":
