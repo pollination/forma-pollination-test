762a41f5f382d0a0b93a1f49ef9b59b351d68c8f
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2023-11-05 02:40:33 -0500

    fix: send the whole proposal without selection

--- assets/forma-binding.js
+++ assets/forma-binding.js
@@ -5,7 +5,7 @@ const scale = [
   [0, 128, 128, 255],
   [0, 255, 0, 255],
   [255, 255, 0, 255],
- [255, 165, 0, 255],
+  [255, 165, 0, 255],
   [255, 0, 0, 255]
 ]
 
@@ -14,31 +14,36 @@ const max = 2;
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
   forma: {
     getVolumes: function (v) {
+      if (!v) return;
       return new Promise(async (resolve) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         const urn = await Forma.proposal.getRootUrn();
-        const paths = await Forma.selection.getSelection();
+        const [terrain] = await Forma.geometry.getPathsByCategory({ category: "terrain" })
+        const builings = await Forma.geometry.getPathsByCategory({ category: "building" })
+
+        const proposalBuildings = builings.filter((path) => path.split("/").length === 2)
+        const surroundingBuildings = builings.filter((path) => path.split("/").length === 3)
+
+        const basePath = surroundingBuildings[0].split("/").slice(0, 2).join("/")
 
         const triangles = [
-          ...(await Forma.geometry.getTriangles({
-            path: "root"
-          })),
-        ];
+          ...await Forma.geometry.getTriangles({ path: terrain }),
+          ...await Forma.geometry.getTriangles({ path: basePath })
+        ]
 
-        if (paths.length) {
+        const volumes = { "type": "FeatureCollection", features: [] };
+        for (let path of proposalBuildings) {
           const geoApi = new EXPERIMENTAL_GeometryApi(Forma.getIframeMessenger());
-          const volumes = await geoApi.EXPERIMENTAL_getVolume25DCollection({
-            urn,
-            path: paths[0],
-          });
-
-          resolve(JSON.stringify({ volumes, triangles }));
-        } else {
-          resolve([]);
+          const featureCollection = await geoApi.EXPERIMENTAL_getVolume25DCollection({ urn, path })
+          volumes.features.push(...featureCollection.features)
         }
+
+        resolve(JSON.stringify({ volumes, triangles }));
       });
     },
     vizualize: function (result, data) {
       return new Promise(async (resolve) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       const resolution = 0.5;
       const position = [];
       const colors = [];
