69dbdf5c4cef65446e5d8e11e89a1d0f80def411
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2023-11-05 02:45:41 -0500

    fix: remove unused inputs

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ from dash import Dash, dcc, html, Input, Output, clientside_callback, callback, 
 from dash.exceptions import PreventUpdate
 app = Dash(__name__, external_scripts=[{"src": "./static/forma.mjs", "type": "module"}])
 
-
-# external_scripts = [{"src": "./assets/forma.js", "type": "module"}]
-
 app.layout = html.Div(
     [
-        html.Div(
-            ["Input: ", dcc.Input(id="my-input", value="initial value", type="text")]
-        ),
-        html.Button("Submit", id="button", n_clicks=0),
+        html.Button("Analyze", id="button", n_clicks=0),
         html.Br(),
-        html.Div(id="my-output"),
         dcc.Store(id="geometry"),
         dcc.Store(id="result", storage_type='local'),
         dcc.Store(id="void")
@@ -39,10 +32,9 @@ clientside_callback(
 def on_data(ts, data):
     if ts is None:
         raise PreventUpdate
-    
     with open("./results.json", "r") as f:
         return json.load(f)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8081)
+    app.run(debug=True, port=8082)
